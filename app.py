f8e138b60ffe04af6b83ef5ba068f9ec7cf55717
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-10 13:08:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import streamlit as st
+import plotly-express as px
+
+df = pd.read_csv("exchange_rates.csv")
 
 st.title("Évolution des taux de change par rapport à l'Euro (€)")
 st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
+
+fig = px.line(df, x="symbol", y="date", hover_name="currency",
+        line_shape="spline", render_mode="svg")
+fig.show()
