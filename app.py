6712b1bb3a6d66cb7dc8fe82295a6adc4fcdcc4a
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-10 13:16:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ df = pd.read_csv("exchange_rates.csv")
 st.title("Évolution des taux de change par rapport à l'Euro (€)")
 st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
 
-fig = px.line(df, x="symbol", y="date", color="currency", hover_name="currency",
+fig = px.line(df, x="symbol", y="value", color="currency", hover_name="date",
         line_shape="spline", render_mode="svg")
 fig.show()
