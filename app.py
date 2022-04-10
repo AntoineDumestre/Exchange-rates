53ac49c53c9e9c8acb091f324126a63dcc6a04b9
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-10 13:00:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-st.title("first app !")
-st.markdown('data credit')
+st.title("Évolution des taux de change par rapport à l'Euro (€)")
+st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
