e46e5a69fe9d096d19c7d8888496cb7db9e5c631
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-10 21:29:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ df = df[df['symbol'].isin(['USD','CHF','GBP','CAD'])]
 st.title("Évolution des taux de change par rapport à l'Euro (€)")
 st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
 
-fig = px.line(df, x="date", y="value", color="symbol", hover="currency",
+fig = px.line(df, x="date", y="value", color="symbol", hover_name="currency",
         line_shape="spline", render_mode="svg")
 st.plotly_chart(fig, use_container_width=True)
