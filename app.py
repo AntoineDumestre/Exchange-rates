56f4dff8f892663834be2196083c6e32c91eb63e
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-13 16:47:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ all_symbols.sort()
 
 # Affichage du graphe
 st.title("Évolution des taux de change par rapport à l'Euro (€)")
-st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
 
 selected_symbols = st.multiselect('Monnaies à afficher', all_symbols, default=['USD','GBP','CHF','CAD'])
 
@@ -19,3 +18,6 @@ df = df[df['symbol'].isin(selected_symbols)]
 fig = px.line(df, x="date", y="value", color="currency", hover_name="currency",
         line_shape="spline", render_mode="svg")
 st.plotly_chart(fig, use_container_width=True)
+
+st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
+
