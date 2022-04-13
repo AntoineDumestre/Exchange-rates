a53c623b21d3947c5e31f1cb9ee135bf31b1a2ad
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-13 17:08:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,12 @@ st.markdown('Date de mise à jour : ' + update_date)
 selected_symbols = st.multiselect('Monnaies à afficher', all_symbols, default=['USD','GBP','CHF','CAD'])
 df = df[df['symbol'].isin(selected_symbols)]
 
-st.select_slider('Choix de la période affichée', options=time_period, value=[df['date'].min(),df['date'].max()])
-
+selected_period = st.select_slider('Choix de la période affichée', options=time_period, value=[df['date'].min(),df['date'].max()])
+df = df[df['symbol'].isin(selected_period)]
           
 fig = px.line(df, x="date", y="value", color="currency", hover_name="currency",
         line_shape="spline", render_mode="svg")
 st.plotly_chart(fig, use_container_width=True)
 
-
-          
 st.markdown('[Data credit : La Banque de France](https://www.banque-france.fr/statistiques/taux-et-cours/les-taux-de-change-salle-des-marches/parites-quotidiennes)')
 
