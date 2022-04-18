593b8cc27aa813b49a1cbc00a154aaa9bdcfd09c
AntoineDumestre <103327756+AntoineDumestre@users.noreply.github.com>
2022-04-18 15:16:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
         st.markdown('#')
         
         max_display_date = df_filtered['date'].max()
-        min_display_date =  df_filtered['date'].min()
+        min_display_date =  df_filtered['date'].min() + timedelta(days=100)
         selected_period = st.select_slider('Choix de la période affichée', options=time_period, value=[min_display_date, max_display_date])
         df_filtered = df_filtered[(df_filtered['date'] >= min(selected_period)) & (df_filtered['date'] <= max(selected_period))]
 
